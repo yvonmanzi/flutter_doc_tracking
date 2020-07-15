83ed3a4295865fb4ae794c62d5a81e259dac4a72
Yvon <ym2637@columbia.edu>
2020-07-15 16:34:14 +0300

    added the final parts of the repository part of the BLoC architecture. next is the logic part

---
+++ lib/src/repository/doc_repository.dart
@@ -0,0 +1,24 @@
+import 'package:doctracking/src/models/doc.dart';
+import 'package:doctracking/src/repository/doc_firestore_client.dart';
+import 'package:meta/meta.dart';
+
+class DocRepository {
+  final DocFirestoreClient _docFirestoreClient;
+
+  DocRepository({@required DocFirestoreClient docFirestoreClient})
+      : assert(docFirestoreClient != null),
+        _docFirestoreClient = docFirestoreClient;
+
+  Future<void> addDocument({@required String title, String expiration}) async {
+    return await _docFirestoreClient.addDocument(
+        title: title, expiration: expiration);
+  }
+
+  Future<void> deleteDocument({@required String title}) async {
+    await _docFirestoreClient.deleteDocument(title: title);
+  }
+
+  Future<List<Doc>> getAllDocuments() async {
+    return await _docFirestoreClient.getAllDocs();
+  }
+}
